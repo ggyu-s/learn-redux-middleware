0b76ebb3867b761e744b6f35ce764b2d476b149d
ggyu <kcpooh0202@naver.com>
2020-12-17 17:09:34 +0900

    learn-redux-middleware"

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./modules";
+// import loggerMiddleware from "./lib/loggerMiddleware";
+import { createLogger } from "redux-logger";
+import ReduxThunk from "redux-thunk";
 
-const store = createStore(rootReducer);
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/modules/sample.js
@@ -0,0 +1,14 @@
+import { handleActions } from "redux-actions";
+import * as api from "../lib/api";
+// 액션 타입을 선언합니다.
+// 한 요청당 세 개를 만들어야 합니다.
+
+const GET_POST = "sample/GET_POST";
+const GET_POST_SUCCESS = "sample/GET_POST_SUCCESS";
+const GET_POST_FAILURE = "sample/GET_POST_FAILURE";
+
+const GET_USERS = "sample/GET_USERS";
+const GET_USERS_SUCCESS = "sample/GET_USERS_SUCCESS";
+const GET_USERS_FAILURE = "sample/GET_USERS_FAILURE";
+
+// thunk 함수를 생성합니다.
