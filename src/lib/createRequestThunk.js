1445240e8f40c94d84ed9d2e0bfd61c6fc51e33c
ggyu <kcpooh0202@naver.com>
2020-12-23 14:39:04 +0900

    redux-middleware

---
+++ src/lib/createRequestThunk.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { startLoading, finishLoading } from "../modules/loading";
+
+function createRequestThunk(type, request) {
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+  return (params) => async (dispatch) => {
+    dispatch({ type }); // 시작됨
+    dispatch(startLoading(type));
+    try {
+      const response = await request(params);
+      dispatch({
+        type: SUCCESS,
+        payload: response.data,
+      });
+      dispatch(finishLoading(type));
+    } catch (e) {
+      dispatch({
+        type: FAILURE,
+        payload: e,
+        error: true,
+      });
+      dispatch(startLoading(type));
+      throw e;
+    }
+  };
+}
+
+export default createRequestThunk;
--- src/modules/sample.js
+++ src/modules/sample.js
@@ -1,5 +1,6 @@
 import { handleActions } from "redux-actions";
 import * as api from "../lib/api";
+import createRequestThunk from "../lib/createRequestThunk";
 // 액션 타입을 선언합니다.
 // 한 요청당 세 개를 만들어야 합니다.
 
@@ -12,3 +13,65 @@ const GET_USERS_SUCCESS = "sample/GET_USERS_SUCCESS";
 const GET_USERS_FAILURE = "sample/GET_USERS_FAILURE";
 
 // thunk 함수를 생성합니다.
+// thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치합니다.
+
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+
+// export const getPost = (id) => async (dispatch) => {
+//   dispatch({ type: GET_POST }); //요청을 시작한 것을 알림
+//   try {
+//     const response = await api.getPost(id);
+//     dispatch({
+//       type: GET_POST_SUCCESS,
+//       payload: response.data,
+//     }); // 요청성공
+//   } catch (e) {
+//     dispatch({
+//       type: GET_POST_FAILURE,
+//       payload: e,
+//       error: true,
+//     }); // 에러발생
+//     throw e; // 나중에 컴포넌트단에서 에러를 조회할 수 있게 해줌
+//   }
+// };
+
+// export const getUsers = () => async (dispatch) => {
+//   dispatch({ type: GET_USERS }); // 요청을 시작한 것을 알림
+//   try {
+//     const response = await api.getUsers();
+//     dispatch({
+//       type: GET_USERS_SUCCESS,
+//       payload: response.data,
+//     });
+//   } catch (e) {
+//     dispatch({
+//       type: GET_USERS_FAILURE,
+//       payload: e,
+//       error: true,
+//     });
+//     throw e;
+//   }
+// };
+
+//초기 상태를 선언
+//쵸청 로딩중 상태는 loading이라는 객체에서 관리합니다.
+const initialState = {
+  post: null,
+  users: null,
+};
+const sample = handleActions(
+  {
+    [GET_POST_SUCCESS]: (state, action) => ({
+      ...state,
+      post: action.payload,
+    }),
+    [GET_USERS_SUCCESS]: (state, action) => ({
+      ...state,
+      users: action.payload,
+    }),
+  },
+  initialState
+);
+
+export default sample;
